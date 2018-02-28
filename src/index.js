02151a095a422506b033e48eb9bad77cd9c318c9
Anna Marfel <anna.marfel@gmail.com>
2018-02-28 21:53:22 +0300

    task is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+
+    let count = 0;
+
+    while (number > 0) {
+        number = Math.floor(number / 5);
+        count += number;
+    }
+    return count;
+
 }
